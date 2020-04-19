eb637f43c7353ff709cf141c7705ea5dd990417a
KosukeShimizu <thimi0412@icloud.com>
2020-04-19 17:45:13 +0900

    モデルとserializerを作成

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Book(models.Model):
+    """本モデル"""
+    class Meta:
+        db_table = 'book'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(varbose='タイトル', max_length=20)
+    price = models.IntegerField(varbose='価格', null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
