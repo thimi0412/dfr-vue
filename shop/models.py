201af3503f71af70dc83db6d7f9ad38af4146cdc
KosukeShimizu <thimi0412@icloud.com>
2020-05-30 02:11:24 +0900

    fix verbose→verbose_name

--- shop/models.py
+++ shop/models.py
@@ -10,8 +10,8 @@ class Book(models.Model):
         db_table = 'book'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(varbose='タイトル', max_length=20)
-    price = models.IntegerField(varbose='価格', null=True)
+    title = models.CharField(verbose_name='タイトル', max_length=20)
+    price = models.IntegerField(verbose_name='価格', null=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
